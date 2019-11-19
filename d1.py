a7f76c8c8826d16826eed07b5ad316beb68816d4
RahmaSandoqa <52237985+RahmaMSandoqa@users.noreply.github.com>
2019-11-19 16:43:14 +0200

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,15 @@
+#Rahma = "hello rahma"
+#print ("Rahman\bmaher")
+
+
+#age = int(input("what is your Age?"))
+#print (age)
+#print (type(age))
+#x, y, z = "rahma","lojen","rashed"
+#print(x)
+#print(y)
+#print(z)
+
+
+
+
---
+++ ex.py
@@ -0,0 +1,125 @@
+#1
+#print ('{:3}  {:6}  {:10}  {:12}'.format('a', 'bc', 'def', 'gh'))
+
+
+
+
+
+
+
+
+
+
+#2 
+#print("\nOrangeAcademy"*20)
+
+#3
+#x=float(1)
+#y=float(2.8)
+#z=float("3")
+#w=float("4.2")
+#print(x,y,z,w,sep='10')
+
+#output(1.0102.8103.0104.2)
+
+
+#4
+#a=10
+#b=20
+#print(a*b)
+
+
+#5
+#a=1
+#b=2
+#c=3
+#d=4
+#e=5
+#print(a+b+c+d+e/5)
+#output 11.0
+#
+#five_values = eval(input("enter five faules"))
+#print(sum(five_values)*len(five_values))
+
+#6
+#x=1
+#y=2.8
+#z=1J
+#o="Orange"
+#A=True
+#print(type(x))
+#print(type(y))
+#print(type(z))
+#print(type(o))
+#print(type(A))
+#output<class 'int'>
+#<class 'float'>
+#<class 'complex'>
+#<class 'str'>
+#<class 'bool'> 
+
+#7
+#x,y="orange",1
+#x1,y1=100,-10
+#print(x)
+#print(y)
+#print(x1)
+#print(y1)
+#outputorange
+#1
+#100
+#-10
+
+#8
+#x5=10
+#print(x5)
+#X_q="Orange"
+#print(X_q)
+#AB=14
+#print(AB)
+#print(10*10)
+
+
+#9
+#x,y="Orange",1
+#X1,y1=100,-10
+#print(x)
+#print(y)
+#print(x1)
+#print(y1)
+
+#10
+
+#11
+#print("Hello,world!")
+#print("cheers,mate!") #this is the program 
+#"""testing
+#the code
+#"""
+
+#12
+#this_year = 2019
+#name_and_age = eval(input("enter you name and age with comma"))
+#print(this_year - name_and_age[1] + 100)
+
+
+#14
+x=11
+y=3
+print(x + y)
+print(x-y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x ** y)
+print(x>y)
+print(x==y)
+print(x!=y)
+
+
